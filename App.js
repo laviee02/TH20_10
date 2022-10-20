e02dedf2cf484cfbc805ad774a2ed978b98d64c9
VietTran <vn@741523.com>
2022-10-20 11:04:37 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,10 @@
+// import * as React from "react";
+import AppNavigation from "./src/navigation/AppNavigation";
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+export default function App() {
+  return (
+    <AppNavigation></AppNavigation>
+  );
+}
---
+++ src/navigation/AppNavigation.js
@@ -0,0 +1,20 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import DetailsScreen from "../screen/DetailsScreen";
+import HomeScreen from "../screen/homescreen";
+import Login from "../screen/login";
+import Register from "../screen/register";
+const Stack = createNativeStackNavigator();
+function AppNavigation() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={Login} />
+        {/* <Stack.Screen name="Register" component={Register} /> */}
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+export default AppNavigation;
